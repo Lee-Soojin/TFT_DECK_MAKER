9518f7d5a2dfa7b83d61e3fd5f0c6c26fb9c2aa7
Soojin <soojl1005@naver.com>
2021-05-25 00:04:58 +0900

    login and signup page background change

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -53,62 +53,64 @@ const Login = ({ authService }) => {
   };
 
   return (
-    <div className={styles.login_container}>
-      <div className={styles.login_wrap}>
-        <h1 className={styles.title}>LOGIN</h1>
-        <form
-          className={styles.form}
-          onSubmit={onMemberLogin}
-          onKeyPress={handleKeyPress}
-        >
-          <div className={styles.email}>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              className={styles.input_email}
-            />
-          </div>
-          <div className={styles.password}>
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              className={styles.input_password}
-            />
-          </div>
-          <button className={styles.BtnLogin} type="submit">
-            <SiRiotgames />
-          </button>
-        </form>
-        <p className={styles.signup_link}>
-          회원이 아니신가요? <Link to="/signup">회원가입</Link>
-        </p>
-        <div className={styles.social_login}>
-          <button
-            type="button"
-            className={styles.BtnGoogleLogin}
-            onClick={onLogin}
+    <div className={styles.body}>
+      <div className={styles.login_container}>
+        <div className={styles.login_wrap}>
+          <h1 className={styles.title}>LOGIN</h1>
+          <form
+            className={styles.form}
+            onSubmit={onMemberLogin}
+            onKeyPress={handleKeyPress}
           >
-            <FcGoogle className={styles.icon_google} />
-            Google
-          </button>
-          <button
-            type="button"
-            className={styles.BtnGithubLogin}
-            onClick={onLogin}
-          >
-            <SiGithub className={styles.icon_github} />
-            Github
-          </button>
+            <div className={styles.email}>
+              <input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                className={styles.input_email}
+              />
+            </div>
+            <div className={styles.password}>
+              <input
+                type="password"
+                placeholder="비밀번호를 입력하세요"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                className={styles.input_password}
+              />
+            </div>
+            <button className={styles.BtnLogin} type="submit">
+              <SiRiotgames />
+            </button>
+          </form>
+          <p className={styles.signup_link}>
+            회원이 아니신가요? <Link to="/signup">회원가입</Link>
+          </p>
+          <div className={styles.social_login}>
+            <button
+              type="button"
+              className={styles.BtnGoogleLogin}
+              onClick={onLogin}
+            >
+              <FcGoogle className={styles.icon_google} />
+              Google
+            </button>
+            <button
+              type="button"
+              className={styles.BtnGithubLogin}
+              onClick={onLogin}
+            >
+              <SiGithub className={styles.icon_github} />
+              Github
+            </button>
+          </div>
         </div>
-      </div>
-      <div className={styles.LoginImage}>
-        <img src={LoginImg} alt="Login_image" />
+        <div className={styles.LoginImage}>
+          <img src={LoginImg} alt="Login_image" />
+        </div>
       </div>
     </div>
   );
--- src/components/login/signup.jsx
+++ src/components/login/signup.jsx
@@ -31,48 +31,50 @@ function SignUp({ authService }) {
     }
   };
   return (
-    <div className={styles.sign_container}>
-      <div className={styles.sign_up_wrap}>
-        <h1 className={styles.title}>Sign Up</h1>
-        <form
-          className={styles.form}
-          onSubmit={handleSubmit}
-          onKeyPress={handleKeyPress}
-        >
-          <div className={styles.email}>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              className={styles.input_email}
-            />
-          </div>
-          <div className={styles.password}>
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              className={styles.input_password}
-            />
-          </div>
-          <button
-            className={styles.BtnSignUp}
-            type="submit"
-            onClick={handleSubmit}
+    <div className={styles.body}>
+      <div className={styles.sign_container}>
+        <div className={styles.sign_up_wrap}>
+          <h1 className={styles.title}>Sign Up</h1>
+          <form
+            className={styles.form}
+            onSubmit={handleSubmit}
+            onKeyPress={handleKeyPress}
           >
-            Sign Up Right now!
-          </button>
-        </form>
-        <p className={styles.login_link}>
-          이미 회원이신가요? <Link to="/login">로그인</Link>
-        </p>
-      </div>
-      <div className={styles.SignUpImage}>
-        <img src={SignUpImage} alt="image_sign_up" />
+            <div className={styles.email}>
+              <input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                className={styles.input_email}
+              />
+            </div>
+            <div className={styles.password}>
+              <input
+                type="password"
+                placeholder="비밀번호를 입력하세요"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                className={styles.input_password}
+              />
+            </div>
+            <button
+              className={styles.BtnSignUp}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Sign Up Right now!
+            </button>
+          </form>
+          <p className={styles.login_link}>
+            이미 회원이신가요? <Link to="/login">로그인</Link>
+          </p>
+        </div>
+        <div className={styles.SignUpImage}>
+          <img src={SignUpImage} alt="image_sign_up" />
+        </div>
       </div>
     </div>
   );
